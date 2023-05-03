b942cadc870b2b57228c4c00ef9bd39dfbc92e7a
yuki383 <yuukikokubo.pg@gmail.com>
2023-05-03 18:35:31 +0900

    install_ubuntu.shにzshのセットアップを記述

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -3,6 +3,10 @@
 DIR=$(cd $(dirname $0); pwd);
 
 # make symboliclink
+if [ ! -e ~/.zshrc  ]; then
+  ln -s "${DIR}/zshrc" "${HOME}/.zshrc"
+fi
+
 if [ ! -e ~/.vimrc  ]; then
   ln -s "${DIR}/vimrc" "${HOME}/.vimrc"
 fi
@@ -16,6 +20,7 @@ if [ ! -e ~/.vim/autoload/plug.vim ]; then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
 
 # apt update
 sudo apt update
@@ -38,4 +43,16 @@ fi
 if !(type "node" > /dev/null 2>&1); then
   pnpm env use --global lts
 fi
+
+# setup zsh
+if !(type "zsh" > /dev/null 2>&1); then
+  sudo apt install zsh
+fi
+
+if !(type "zplug" > /dev/null 2>&1); then
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
+
+# デフォルトシェルをzshに変更
+chsh -s $(which zsh)
 
